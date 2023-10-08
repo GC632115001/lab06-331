25aa8f4181dbd5a35e8260129500aa90e619f6dc
GC632115001 <poison45lol@gmail.com>
2023-10-08 21:15:48 +0700

    fix 2.5

--- .env.development.ts
+++ .env.development.ts
@@ -1,2 +1,1 @@
 VITE_BACKEND_URL=http://localhost:3004
-VUE_APP_BACKEND_URL= https://my-json-server.typicode.com/GC632115001/lab06-331
---
+++ .env.production.ts
@@ -0,0 +1,1 @@
+VUE_APP_BACKEND_URL= https://my-json-server.typicode.com/GC632115001/lab06-331
